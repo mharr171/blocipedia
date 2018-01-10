3757df7994fe1449af18b680c899627bbac3214d
Matthew Harris <mharr171.z@gmail.com>
2018-01-10 05:31:21 -0800

    ActionView Template Error

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,5 +53,18 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Devise
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    user_name:      'mharr171.z',
+    password:       '$mGHMaa! 6081',
+    domain:         'localhost:3000',
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
